551b31daca85a4e6317d39834ea9af0418ddc8a1
tonysmith1sme <tonysmith1sme@gmail.com>
2026-09-24 21:38:38 +0800

    chore(camera): drop android-stub along with origin's desktop-test removal
    The desktop protocol tests (and the android-stub shim they needed to
    compile RecSession off-device) were deleted upstream in 2.5; finish
    removing the leftover stub tree.

--- Camera/desktop-test/android-stub/android/graphics/ImageFormat.java
+++
@@ -1,5 +0,0 @@
-package android.graphics;
-
-public class ImageFormat {
-    public static final int NV21 = 17;
-}
--- Camera/desktop-test/android-stub/android/graphics/Rect.java
+++
@@ -1,5 +0,0 @@
-package android.graphics;
-
-public class Rect {
-    public Rect(int l, int t, int r, int b) {}
-}
